9c7782f7c924a965930671a538d2311b5413dd1f
Polymatio <jss.prras@outlook.com>
2022-08-18 15:57:19 -0700

    Pushing a function in progress

---
+++ JavaScript/Pythagorean Triple/js/main.js
@@ -0,0 +1,12 @@
+// Create a function that determines if the non-negative integers form a pythagorean triple.
+
+function isPythagoreanTriple(integers) {
+    for(let i = 1; i < integers - 1; i++) {
+        for(let j = i; b < integers; j++) {
+            k = Math.sqrt(i * i + j * j);
+            if(c % 1 === 0) {
+                return integers
+            }
+        }
+    }
+}
