e4fd371dfc0479610f33bded2cffa0ff18d88d38
Polymatio <jss.prras@outlook.com>
2022-08-23 20:43:13 -0700

    Created a function to determine if they form a pythagorean triple

--- JavaScript/Pythagorean Triple/js/main.js
+++ JavaScript/Pythagorean Triple/js/main.js
@@ -1,12 +1,6 @@
 // Create a function that determines if the non-negative integers form a pythagorean triple.
 
 function isPythagoreanTriple(integers) {
-    for(let i = 1; i < integers - 1; i++) {
-        for(let j = i; b < integers; j++) {
-            k = Math.sqrt(i * i + j * j);
-            if(c % 1 === 0) {
-                return integers
-            }
-        }
-    }
+    let num = integers.sort((a, b) => a - b)
+    return (num[0] ** 2 + num[1] ** 2 == num[2] ** 2)
 }
