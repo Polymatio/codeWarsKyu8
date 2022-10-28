81fd33166c13ffc61761d8577f55a39460e1ceae
Jesus Porras <102770370+Polymatio@users.noreply.github.com>
2022-10-27 20:13:14 -0700

    Create a function to determine if array have value

---
+++ JavaScript/You only need one - Beginner/js/main.js
@@ -0,0 +1,6 @@
+// create a function to return an array containing a value
+// The include method will be used to check if the array haves a value when returned
+
+function check(a, x) {
+    return a.includes(x);
+}
